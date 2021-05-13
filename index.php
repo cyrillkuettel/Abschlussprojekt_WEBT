44c40e846e4a5f119ef9587a3c2b0e20a30a762d
Cyrill <cyrillkuettel@gmail.com>
2021-05-13 21:40:43 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Home</title>
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+
+    <!--- Hier drin: Information zu Webapplikation und wie der Benutzer die Applikation zu bedienen hat.-->
+    <h1 id="Information">Information</h1>
+    <p style="margin-bottom:800px">
+        Auch noch etwas Raum.
+    </p>
+    <h1 id="Formular"> Formular</h1>
+    <p style="margin-bottom:800px">
+        Auch noch etwas Raum.
+    </p>
+    <h1 id="Canvas"> Canvas</h1>
+    <p style="margin-bottom:800px">
+        Auch noch etwas Raum.
+    </p>
+    <h1 id="Gästebuch"> Gästebuch</h1>
+    <h2 class="text-white mt-0">Hier kannst du einen Gästebucheintrag hinterlassen.</h2>
+    <?php include('eintrag_erstellen.php'); ?>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar" id="mainNav">
+
+    <ul class="navbar">
+        <li class="nav-item"><a href="#Information">Information</a></li>
+        <li class="nav-item"><a href="#Formular">Formular</a></li>
+        <li class="nav-item"><a href="#Canvas">Canvas</a></li>
+        <li class="nav-item"><a href="#Gästebuch">Gästebuch</a></li>
+    </ul>
+
+</nav>
