35525223161ed8e6f7c0fa2523edd5d3e31aa7ae
cyrillkuettel <cyrillkuettel@gmail.com>
2021-06-04 15:18:51 +0200

    massive restrukturierung der php files

--- eintrag_form.php
+++ eintrag_form.php
@@ -45,65 +45,105 @@ $name_error = $email_error = $phone_error = $url_error = "";
 $name = $email = $phone = $message = $url = $success = "";
 
 require_once 'GuestbookAccess.php';
-//form is submitted with the post method:
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST['name'])) {
-        echo '<script> alert("Geben Sie gültige Eingaben in das Formular!");  </script>';
-    } else {
-        $name = test_input($_POST["name"]);
-        if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
-            $name_error = "Only letters and white space allowed";
+switch ($_POST['action']) {
+    case "buy":
+        if (empty($_POST['name'])) {
+            echo '<script> alert("Geben Sie gültige Eingaben in das Formular!");  </script>';
+        } else {
+            $name = test_input($_POST["name"]);
+            if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
+                $name_error = "Only letters and white space allowed";
+            }
         }
-    }
 
-    if (empty($_POST["email"])) {
-        $email_error = "Email is required";
-    } else {
-        $email = test_input($_POST["email"]);
-        //check if e-mail address is well-formed
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $email_error = "invalid email format";
+        if (empty($_POST["email"])) {
+            $email_error = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            //check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $email_error = "invalid email format";
+            }
         }
-    }
 
-    if (empty($_POST["message"])) {
-        $message = "";
-    } else {
-        $message = $_POST["message"];
-    }
-
-    if (empty($_POST["message"])) {
-        $message = "";
-    } else {
-        $message = test_input($_POST["message"]);
-    }
-
-
-    $message_body = '';
-    if ($name_error == '' and $email_error == '') {
-
-        unset($_POST['submit']);
-
-        foreach ($_POST as $key => $value) {
-            $message_body .= "$key: $value\n";
+        if (empty($_POST["message"])) {
+            $message = "";
+        } else {
+            $message = $_POST["message"];
         }
-        //$str = str_replace(array("\r", "\n"), '', $str);
-        // debug(str_replace(array("\r", "\n"), ' ', $message_body));
 
-        // Everything is filled out. Now put the information in the database
-        // Create an object of the GuestbookAccess class
-        $guestbook = new GuestbookAccess();
+        if (empty($_POST["message"])) {
+            $message = "";
+        } else {
+            $message = test_input($_POST["message"]);
+        }
 
-        // Call the add entry methode
-        // The methode itself adds an unique index and the date to the entry.
-        $id1 = $guestbook->addEntry($name, $email, $message);
+        break;
+        
+        // Hier wird der Gästebucheintrag gehandelt
+    case "entry":
 
-        // echo "Added new entry with index $id1\n";
+        if (empty($_POST['name'])) {
+            echo '<script> alert("Geben Sie gültige Eingaben in das Formular!");  </script>';
+        } else {
+            $name = test_input($_POST["name"]);
+            if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
+                $name_error = "Only letters and white space allowed";
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $email_error = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            //check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $email_error = "invalid email format";
+            }
+        }
+
+        if (empty($_POST["message"])) {
+            $message = "";
+        } else {
+            $message = $_POST["message"];
+        }
+
+        if (empty($_POST["message"])) {
+            $message = "";
+        } else {
+            $message = test_input($_POST["message"]);
+        }
 
 
-    }
-} //if $_SERVER["REQUEST_METHOD"] == "POST") endet hier
+        $message_body = '';
+        if ($name_error == '' and $email_error == '') {
+
+            unset($_POST['submit']);
+
+            foreach ($_POST as $key => $value) {
+                $message_body .= "$key: $value\n";
+            }
+            //$str = str_replace(array("\r", "\n"), '', $str);
+            // debug(str_replace(array("\r", "\n"), ' ', $message_body));
+
+            // Everything is filled out. Now put the information in the database
+            // Create an object of the GuestbookAccess class
+            $guestbook = new GuestbookAccess();
+
+            // Call the add entry methode
+            // The methode itself adds an unique index and the date to the entry.
+            $id1 = $guestbook->addEntry($name, $email, $message);
+
+            // echo "Added new entry with index $id1\n";
+
+
+        }
+        break;
+}
+
+
+
 
 function test_input($data)
 {
