f3843a4e8e066e3f67bf9c5cf2c60510fec08efc
Cyrill <cyrillkuettel@gmail.com>
2021-06-05 00:45:40 +0200

    major changes

--- eintrag_form.php
+++ eintrag_form.php
@@ -38,115 +38,82 @@
 </html>
 
 
+
+
+
+
+
 <?php
 //define variables and set them to empty values
 //now check if all the entries are valid
-$name_error = $email_error = $phone_error = $url_error = "";
-$name = $email = $phone = $message = $url = $success = "";
 
+
+
+$name_error = $email_error = $phone_error = $url_error = "";
+$name = $email = $velo = "";
+
+/*
+because of this error:
+    the " PHPSESSID " cookie will soon be rejected because 
+    its " sameSite " attribute is set to " none " or an invalid value, and without " secure " attribute. 
+*/
 
 
 require_once 'GuestbookAccess.php';
 
-switch ($_POST['action']) {
-    case "buy":
-        // TODO:  use isset to check null as well. 
-        if (empty($_POST['name'])) {
-            warn_and_go_back("Geben Sie gültige Eingaben in das Formular!");
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    debug("got here");
+    // TODO:  use isset to check null as well. 
+    if (empty($_POST['name'])) {
+        warn_and_go_back("Geben Sie gültige Eingaben in das Formular!");
+        return;
+    } else {
+        $name = $_POST["name"];
+        if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
+            warn_and_go_back("Nur Buchstaben und Leerschläge im Namen!");
+        }
+    }
+    if (empty($_POST["email"])) {
+        warn_and_go_back("Bitte geben Sie eine Email an");
+        return;
+    } else {
+        $email = $_POST["email"];
+        //check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            warn_and_go_back("Diese Email ist ungültig");
             return;
+        }
+    }
+    if (!empty($_POST['option'])) {
+        if ($_POST['option'] == "scm1") { // ausverkauft
+            // Construct Message für ausverkauft. 
+            // -> Hier ein seperates HTML machen und schnell dort prototypen
         } else {
-            $name = $_POST["name"];
-            if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
-                warn_and_go_back("Nur Buchstaben und Leerschläge im Namen!");
-            }
+            $velo = $_POST['option'];
         }
+    } else {
+        warn_and_go_back("Kein bike ausgewählt?");
+    }
 
-        if (empty($_POST["email"])) {
-            // echo '<script> alert("Geben Sie eine Email an 1"); window.location = "index.html"; </script>';
-            warn_and_go_back("Bitte geben Sie eine Email an");
-            return;
-        } else {
-            $email = $_POST["email"];
-            //check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                warn_and_go_back("Diese Email ist ungültig");
-                return;
-            }
-        }
-        if (!empty($_POST['option'])) {
-            if ($_POST['option'] == "scm1") { // ausverkauft
-                
-            } else {
+    //  ------------------------------------
 
 
-            }
-        } else {
-            warn_and_go_back("Kein bike ausgewählt?");
-        }
+    // unset($_POST['submit']);
 
+    // Everything is filled out. Now put the information in the database
+    // Create an object of the GuestbookAccess class
+    $guestbook = new GuestbookAccess();
 
-        break;
+    // Call the add entry methode
+    // The methode itself adds an unique index and the timestamp to the entry.
+    $id1 = $guestbook->addEntry($name, $email, $velo);
 
-        //  ------------------------------------------  Hier wird der Gästebucheintrag gehandelt
-    case "entry":
+     echo "Added new entry with index $id1\n";
 
-        if (empty($_POST['name'])) {
-            echo '<script> alert("Geben Sie gültige Eingaben in das Formular!");  </script>';
-        } else {
-            $name = $_POST["name"];
-            if (!preg_match("/^[a-zA-Z\s]*$/i", $name)) {
-                $name_error = "Only letters and white space allowed";
-            }
-        }
-
-        if (empty($_POST["email"])) {
-            $email_error = "Email is required";
-        } else {
-            $email = $_POST["email"];
-
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $email_error = "invalid email format";
-            }
-        }
-
-        if (empty($_POST["message"])) {
-            $message = "";
-        } else {
-            $message = $_POST["message"];
-        }
-
-        if (empty($_POST["message"])) {
-            $message = "";
-        } else {
-            $message = $_POST["message"];
-        }
-
-
-        $message_body = '';
-        if ($name_error == '' and $email_error == '') {
-
-            unset($_POST['submit']);
-
-            foreach ($_POST as $key => $value) {
-                $message_body .= "$key: $value\n";
-            }
-            //$str = str_replace(array("\r", "\n"), '', $str);
-            // debug(str_replace(array("\r", "\n"), ' ', $message_body));
-
-            // Everything is filled out. Now put the information in the database
-            // Create an object of the GuestbookAccess class
-            $guestbook = new GuestbookAccess();
-
-            // Call the add entry methode
-            // The methode itself adds an unique index and the date to the entry.
-            $id1 = $guestbook->addEntry($name, $email, $message);
-
-            // echo "Added new entry with index $id1\n";
-
-
-        }
-        break;
+} else {
+    debug("got to the exit");
 }
+
 # Cookie erstellen
 if (!(isset($_COOKIE['customerName']))) {
     if (!empty($name)) {
--- schema.sql
+++ schema.sql
@@ -2,4 +2,4 @@ create database db1;
 
 use db1;
 
-create table guestbook(indes int NOT NULL AUTO_INCREMENT , cur_date time DEFAULT current_timestamp(), namep varchar(100), veloType varchar(1000) );
+create table guestbook (indes int NOT NULL AUTO_INCREMENT, cur_date time DEFAULT current_timestamp(), namep varchar(50), email varchar(320), veloType varchar(1000), PRIMARY KEY (indes));
