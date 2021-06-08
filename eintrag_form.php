4bf1f5e6e30b77d291b3285476fa564b8e0faace
cyrillkuettel <cyrillkuettel@gmail.com>
2021-06-08 11:19:49 +0200

    refactoring to use prepared statements

--- eintrag_form.php
+++ eintrag_form.php
@@ -32,9 +32,6 @@
     </nav>
 </body>
 
-
-
-
 <?php
 //define variables and set them to empty values
 //now check if all the entries are valid
@@ -46,7 +43,120 @@ $name = $email = $velo = "";
 
 
 
-require_once 'AccessDB.php';
+class AccessDB
+{
+    private $db;
+    private $table;
+
+    public function __construct()
+    {
+        $username = "root";
+        $password = "";
+        $database = "db1";
+        $this->table = "guestbook";
+        
+        $this->db = mysqli_connect("localhost", $username, $password);        
+        if ($this->db == false) {
+            die("Unable to connect to database");
+        } 
+        
+        // Select database
+        mysqli_select_db($this->db, $database);
+    }
+    
+    public function __destruct()
+    {
+        mysqli_close($this->db);
+    }
+    
+    public function getEntries()
+    {
+        // Make querry
+        $t = $this->table;
+        $result = mysqli_query($this->db, "SELECT * FROM $t");
+        
+        $table = false;
+        $i = 0;
+        while ($row = mysqli_fetch_array($result)) {
+            $table[$i]["Index"]   = $row["indes"];
+            $table[$i]["Date"]    = $row["cur_date"];
+            $table[$i]["Name"]    = $row["namep"];
+          //  $table[$i]["eMail"]   = $row["email"]; // not displaying email in public
+            $table[$i]["veloType"] = $row["veloType"];
+            $i++;
+        }
+        
+        mysqli_free_result($result);
+        
+        return $table;
+    }
+    
+
+    function addEntry($name, $eMail, $veloType)
+    {   
+        
+        function debugthis($data) {
+            $output = $data;
+            if (is_array($output))
+                $output = implode(',', $output);
+        
+             echo "<script>console.log('text " . $output . "' );</script>";
+        }
+        
+
+        
+
+        // Add entry to the database
+        $t = $this->table; // local variable, because I could not access the field "$this->table" in the subsequent line
+        $stmt = $this->db->prepare("INSERT INTO $t (namep, email, veloType) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $name, $email, $veloType);
+
+       // $result = mysqli_query($this->db, "INSERT INTO $t (namep, email, veloType) VALUES ('$name', '$eMail', '$veloType')");
+        
+
+        // Is this necessary, if prepared statements are being used? 
+        //  I suppose not. "If the original statement template is not derived from external input, SQL injection cannot occur."
+
+        $name    = mysqli_real_escape_string($this->db, $name);
+        $eMail   = mysqli_real_escape_string($this->db, $eMail);
+        $veloType = mysqli_real_escape_string($this->db, $veloType);
+
+        
+
+        if ($result)
+        {
+            $result = mysqli_insert_id($this->db);
+        } else {
+           debug("Failed to add entry.");
+        }
+
+        return $result;
+    }
+
+    public function getEntry($index)
+    {
+
+        settype($index, 'Integer');
+
+        // Make query
+        $t = $this->table;
+        $result = mysqli_query($this->db, "SELECT * FROM $t WHERE indes = '$index'");
+        
+        $list = false;
+        $row = mysqli_fetch_array($result);
+        if ($row != false) {            
+            $list["Index"]   = $row["indes"];
+            $list["Date"]    = $row["cur_date"];
+            $list["Name"]    = $row["namep"];
+            $list["eMail"]   = $row["email"];
+            $list["veloType"] = $row["veloType"];
+        }
+        
+        mysqli_free_result($result);
+        
+        return $list;
+    }
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // TODO:  use isset to check null as well. 
@@ -222,9 +332,11 @@ function debug($data)
     </div>
 
 </section>
-<footer class="w3-container">
-        <div class="w3-container w3-center">
-            <p class="copyright"> HSLU FS2021 - Cyrill Küttel Copyright &copy;</p>
-        </div>
+<footer>
+        <div class="footer-container">
+            <div class="footer-center">
+                <p index="copyright"> HSLU FS2021 - Cyrill Küttel Copyright &copy;</p>
+            </div>
+          </div>
     </footer>
 </html>
